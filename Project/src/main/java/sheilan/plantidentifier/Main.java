5978cc691045315b387d44ab9e37a172fefcbfa6
Sheila Nicholson <aleihsdn@gmail.com>
2024-09-28 13:02:37 -0700

    created .gitignore

---
+++ Project/src/main/java/sheilan/plantidentifier/Main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sheilan.plantidentifier;
+// import java.net.URI;
+// import java.net.http.HttpClient;
+
+// import java.net.http.HttpRequest;
+// import java.net.http.HttpResponse;
+// import java.io.IOException;
+
+/**
+ *
+ * @author sheil
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
